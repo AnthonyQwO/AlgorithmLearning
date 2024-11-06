ec98c98fc8e12bd6ff64cf7a3ef17fb0fed233c4
AnthonyQwO <104832831+AnthonyQwO@users.noreply.github.com>
2024-11-07 03:39:21 +0800

    Update MergeSort.cpp

--- Sort/MergeSort.cpp
+++ Sort/MergeSort.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge( vector<int> &arr, int l, int m, int r ) { //[l,r)
-    vector<int> temp = arr;
-    reverse(temp.begin() + m, temp.begin() + r);
-    for( int i=l, j=r-1, k=l ; k < r ; k++ ) {
-        if( temp[i] < temp[j] )
-            arr[k]=temp[i++];       
+void merge(vector<int> &arr, int l, int m, int r) { // [l, r)
+    vector<int> temp(arr.begin() + l, arr.begin() + r);
+    reverse(temp.begin() + m - l, temp.begin() + r - l);
+    for( int i = l, j = r - 1, k = l ; k < r; k++) {
+        if(temp[i - l] < temp[j - l])
+            arr[k] = temp[i++  - l];       
         else
-            arr[k]=temp[j--];
+            arr[k]=temp[j-- - l];
     }
 }
 
-void merge_sort( vector<int> &arr, int l, int r ) { //[l,r)
-    if( r-l >=2 ) {
-        int m = (l+r)/2;
-        merge_sort(arr,l,m);
-        merge_sort(arr,m,r);
-        merge(arr,l,m,r);
+void mergeSort(vector<int> &arr, int l, int r) { // [l, r)
+    if(r - l >= 2) {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m, r);
+        merge(arr, l, m, r);
     }
 }
 
-int main()
-{
-    vector<int> arr={INT32_MAX,12,2313,-121212,3232,1,2,0,22,4};
-    merge_sort(arr,0,10);
-    for( int i=0 ; i < arr.size() ; i++ )
-        cout<<arr[i]<<' ';
-
+int main() {
+    vector<int> arr = {INT32_MAX, 12, 2313, -121212, 3232, 1, 2, 0, 22, 4};
+    mergeSort(arr, 0, 10);
+    for(int i: arr) cout << i << ' ';
 }
